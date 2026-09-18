4b9a77f0bba5b8896b0e7cd7949f9ea58a309fc2
Harvey Tseng <harveytseng2@gmail.com>
2026-09-17 20:41:41 -0400

    chore: regenerate the demo packages

--- soothfast-demo/bindings/ruby/ext/soothfast_stats/src/lib.rs
+++ soothfast-demo/bindings/ruby/ext/soothfast_stats/src/lib.rs
@@ -7,7 +7,7 @@ use ::magnus::prelude::*;
 static ERROR: ::magnus::value::Lazy<::magnus::ExceptionClass> = ::magnus::value::Lazy::new(|ruby| {
     ruby.define_module("SoothfastStats")
         .and_then(|m| m.define_error("Error", ruby.exception_standard_error()))
-        .expect("defines SoothfastStats::Error")
+        .unwrap_or_else(|e| panic!("defines SoothfastStats::Error: {e}"))
 });
 
 fn metric_from_symbol(
